907b4187090003ffdbe82fba9b5fc79ea0c218b1
Michael Groberman <michaelgroberman@MacBook-Pro.local>
2021-08-09 15:10:58 -0400

    updated index.js
    created the array of questions to ask the user

--- index.js
+++ index.js
@@ -5,7 +5,53 @@ const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile)
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message:
+          "Please enter your GitHub username",
+        name: "username",
+      },
+      {
+        type: "input",
+        message: "What is your Repo/Project's Name?",
+        name: "projectName",
+      },
+      {
+        type: "input",
+        message:
+          "Provide a description of the project",
+        name: "description",
+      },
+      {
+        type: "list",
+        message: "What kind of license do you want for the project?",
+        name: "license",
+        choices: ["MIT", "Gpl-3.0", "Unlicense", "Apache-2.0", "None"],
+      },
+      {
+        type: "input",
+        message:
+          "What do you want to include in the Usage Section of your project?",
+        name: "usage",
+      },
+      {
+        type: "input",
+        message:
+          "Installation instructions for your project:",
+        name: "install",
+      },
+      {
+        type: "input",
+        message:
+          "What do you want the user to know about contributing to your poject?",
+        name: "contributing",
+      },
+      {
+        type: "input",
+        message: "How can the user run tests for your project?",
+        name: "tests",
+      }]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
