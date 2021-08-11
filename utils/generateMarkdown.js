10c07a9644b3d83afc075c785f4ffaef1fbaab16
Michael Groberman <michaelgroberman@MacBook-Pro.local>
2021-08-10 21:11:05 -0400

    updated js
    created a function to return a license badge based on which license is selected. I created a function that returns the license link. I added the markup to be added to the readme and included the objects stored after prompting the user our questions with inquirer

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,64 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in. If there is no license, it returns an empty string
+function renderLicenseBadge(license) {
+  var badge = "";
+  if (license) {
+    badge = `[![License: ${license}](https://img.shields.io/badge/License-${encodeURI(license)}-blue.svg)](${renderLicenseLink(license)})`;
+  }
+  return badge;
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+}
+// Function that returns the license link. If there is no license, it return an empty string.
+function renderLicenseLink(license) {
+  var link = "";
+  if (license) {
+    link = `https://opensource.org/licenses/${encodeURI(license)}`
+  }
+  return link;
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function that returns the license section of README. If there is no license, it returns an empty string.
+function renderLicenseSection(license) {
+  var section = "";
+  if (license) {
+    section = `Licensed under the [${license}](${renderLicenseLink(license)}) license.`;
+  }
+  return section;
+}
 
-// TODO: Create a function to generate markdown for README
+// Function that generates markdown for README file
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  const content = `
+  # ${data.title}  
+  ${renderLicenseBadge(data.licenseType)} 
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  ## Installation
+  ${data.installInstructions}
+  
+  ## Usage
+  \'''bash
+    ${data.appUsage}
+  \'''
+  
+  ## License
+  Copyright (c) ${data.gitHubUser}. All rights reserved.
+  ${renderLicenseSection(data.licenseType)} 
+  
+  ## How to Contribute
+  ${data.howToContribute}
+  ## Tests  
+  ${data.testInstructions}
+  ## Questions?
+  GitHub [${data.gitHubUser}](https://github.com/${data.gitHubUser})
+  How to reach me with additional questions ? - [${data.userEmail}](mailto://${data.userEmail})
 `;
+
+  return content;
 }
 
 module.exports = generateMarkdown;
